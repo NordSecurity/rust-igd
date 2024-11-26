83bcc2d8c156593fc2df2596e8e0425da60700ac
Tomasz Klak <tomasz.klak@nordsec.com>
2024-11-26 14:41:28 +0100

    Fix test_parse_device2 by moving Processing Instruction to first line
    Processing instructions need to be at the beginning of the document.

--- src/common/parsing.rs
+++ src/common/parsing.rs
@@ -507,8 +507,7 @@ fn test_parse_device1() {
 
 #[test]
 fn test_parse_device2() {
-    let text = r#"
-    <?xml version="1.0" ?>
+    let text = r#"<?xml version="1.0" ?>
     <root xmlns="urn:schemas-upnp-org:device-1-0">
         <specVersion>
             <major>1</major>
